da3d9ae26a689890f28ec8481b55ab0664cb8f6c
Doguran <doguran@yandex.ru>
2019-05-26 23:55:21 +0400

    26.05.2019

--- app/js/main.js
+++ app/js/main.js
@@ -3,3 +3,13 @@
 //= ../../node_modules/popper.js/dist/umd/popper.js
 
 //= ../../node_modules/bootstrap/dist/js/bootstrap.js
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() >= 290) {
+            $('.mobil').addClass('stickytop');
+        }
+        else{
+            $('.mobil').removeClass('stickytop');
+        }
+    });
+
